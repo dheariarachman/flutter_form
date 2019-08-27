1ea1990b75da6cec1a54852494a03253ca8b8eb0
Dhea Aria Rachman <dheariarachman@gmail.com>
2019-08-27 13:39:26 +0700

    Fix Clear Data

--- lib/main.dart
+++ lib/main.dart
@@ -66,7 +66,7 @@ class _MyHomePageState extends State<MyHomePage> {
               Icons.clear,
               color: Colors.white,
             ),
-            onPressed: () => _resetCarts,
+            onPressed: () => _resetCarts(),
           )
         ],
       ),
